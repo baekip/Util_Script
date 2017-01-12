d3fa8327bef38d58972418da7d6d102b05dc3e02
inpyo.baek <baekip@soybean.ptbio.kr>
2017-01-12 17:17:37 +0900

    generate sequencing stat file script

--- util/seqtk_of_customer_size.pl
+++ util/seqtk_of_customer_size.pl
@@ -2,125 +2,174 @@
 
 use strict;
 use warnings;
+use Getopt::Long;
 use File::Basename;
 use Cwd qw(abs_path);
 
-my $script_path = dirname( abs_path $ARGV[0]);
-my $orig_rawdata_path = "$script_path/result/01_fastqc_orig/";
+my $in_config;
+GetOptions(
+    'config=s' => \$in_config,
+);
 
-if (@ARGV != 1){
-    printUsage();
+if (!defined $in_config or !-f $in_config){
+    die "ERROR! check your config file with -c option\n";
 }
 
-my $in_config = $ARGV[0];
+print $in_config."\n";
 
+my $config_path = dirname (abs_path $in_config);
+$in_config="$config_path/$in_config";
 my %info;
-read_config ($in_config, \%info);
+read_general_config ($in_config, \%info);
 
-my $project_path = $info{project_path};
-my $sample_id = $info{sample_id};
-my $additional_bp_range = $info{additional_bp_range};
-my @list_sample_id = split /\,/, $sample_id;
+#############################################################
+#0.preparation
+#############################################################
+
+my $project_path=$info{project_path};
+my $project_id=$info{project_id};
+my $telomeric_path="$project_path/result/00_telo-seq";
+my $delivery_id=$info{delivery_tbi_id};
+my @delivery_list=split /\,/, $delivery_id;
+
+#############################################################
+#1.read fastq stats xls file 
+#############################################################
+my $rawdata_path="$project_path/rawdata";
+my $sequence_stat="$rawdata_path/Sequencing_Statistics_Result.xls";
+my $seqtk_path="$project_path/result/01-1_seqtk_run";
+my $fastq_path="$project_path/result//01_fastqc_orig";
 
 
-my $seqtk_exe = "/BiO/BioTools/seqtk/dev/seqtk";
+open my $fh_stat, '>', $sequence_stat or die;
+print $fh_stat "SampleID\tIndex\tTotalReads\tTotalBases\tTotalBases(Gb)\tGC_Count\tGC_Rate N_ZeroReads\tN_ZeroReadsRate\tN5_LessReads\tN5_LessReadsRate\tN_Count N_Rate\tQ30_MoreBases\tQ30_MoreBasesRate\tQ20_MoreBases\tQ20_MoreBasesRate\n";
 
-my $fastqc_path = "$project_path/result/01_fastqc_orig";
-my $sh_dir = "$project_path/result/sh_log_file";
-my $result_seqtk = "$project_path/result/00_seqtk_cut";
-my $sh_seqtk = "$sh_dir/00_seqtk_cut";
-make_dir($sh_seqtk);
+foreach my $id (@delivery_list) {
+    my ($delivery_id,$tbi_id,$type_id) = split /\:/, $id;
+    my $sample_seqtk_path="$seqtk_path/$tbi_id/";
+    checkDir($sample_seqtk_path);
+    my $rst_xls="$sample_seqtk_path/$tbi_id\_1.fastq.gz.rst.xls";
+    checkFile($rst_xls);
+       
+    #####Read column value#####
+    my $total_reads = `cat $rst_xls | grep -v \'#\'| cut -f 1 `;
+    chomp ($total_reads);
+    my $total_length = `cat $rst_xls | grep -v \'#\'| cut -f 2 `;
+    chomp ($total_length);
+    my $total_GC = `cat $rst_xls | grep -v \'#\'| cut -f 3 `;
+    chomp ($total_GC);
+    my $Nzero_reads = `cat $rst_xls | grep -v \'#\'| cut -f 4 `;
+    chomp ($Nzero_reads);
+    my $N5_reads = `cat $rst_xls | grep -v \'#\' | cut -f 5 `;
+    chomp ($N5_reads);
+    my $N_count = `cat $rst_xls | grep -v \'#\' | cut -f 6 `;
+    chomp ($N_count);
+    my $Q30_base_R1 = `cat $rst_xls | grep -v \'#\' | cut -f 9`;
+    chomp ($Q30_base_R1);
+    my $Q30_base_R2 = `cat $rst_xls | grep -v \'#\' | cut -f 10`;
+    chomp ($Q30_base_R2);
+    my $Q20_base_R1 = `cat $rst_xls | grep -v \'#\' | cut -f 13`;
+    chomp ($Q20_base_R1);
+    my $Q20_base_R2 = `cat $rst_xls | grep -v \'#\' | cut -f 14`;
+    chomp ($Q20_base_R2);
 
-
-#/BiO/BioProjects/GREECE-Human-WES-2015-10-TBO150073/result/01_fastqc_orig/TN1509D0878
-#qsub -cwd -S /usr/bin/perl
-#fastqc.TN1509D0878.1.sh
-
-foreach (@list_sample_id) {
-    my ($tbi_id, $customer_size) = split/\:/, $_;
+    #####correct stats column#####
     
+    my $sample_rawdata="$fastq_path/$tbi_id/$tbi_id\_1.fastq.gz";
+    checkFile($sample_rawdata);
+    my $fastq_header = `zcat $sample_rawdata | head -n 1 `;
+    chomp ($fastq_header);
+    my @header_list = split /\:/, $fastq_header;
+    my $index=$header_list[-1];
     
-    my $sample_result_dir = "$result_seqtk/$tbi_id";
-    make_dir($sh_seqtk);
-    my $sh_log_dir = "$sh_seqtk/$tbi_id";
-    make_dir($sh_log_dir);
-    my $rawdata_path = "$project_path/rawdata/$tbi_id";
-    make_dir($rawdata_path);
-    
-    my $half_size = $customer_size/2;
-    my $customer_bp = $half_size * 1000000000;
-    my $customer_line = int ( ($customer_bp / 101) * ($additional_bp_range + 1) )  ; 
-    
-    my $fastqc_raw_1 = "$fastqc_path/$tbi_id/$tbi_id"."_1.fastq.gz";
-    my $fastqc_raw_2 = "$fastqc_path/$tbi_id/$tbi_id"."_2.fastq.gz";
-   
-    my $new_read_1 = "$rawdata_path/$tbi_id"."_1.fastq.gz";
-    my $new_read_2 = "$rawdata_path/$tbi_id"."_2.fastq.gz";
-
-    print "Trimming of $fastqc_raw_1: $half_size Gbp \n";
-    print "Trimming of $fastqc_raw_2: $half_size Gbp \n";
-
-    my $seqtk_sh_1 = "$sh_log_dir/seqtk.$tbi_id"."_1.sh";
-    my $seqtk_sh_2 = "$sh_log_dir/seqtk.$tbi_id"."_2.sh";
-    
-
-    open my $fh_sh_1, '>', $seqtk_sh_1 or die;
-    print $fh_sh_1 "date \n";
-    my $command_1 = "$seqtk_exe sample -2 -s100 $fastqc_raw_1 $customer_line | gzip -c > $new_read_1 \n";
-    print $fh_sh_1 $command_1;
-    print $fh_sh_1 "date \n";
-    close $fh_sh_1;
-    my $cmd_qsub_sh_1 = "qsub -V -e $sh_log_dir -o $sh_log_dir -S /bin/bash $seqtk_sh_1";
-    system($cmd_qsub_sh_1);
-    print $command_1."\n";
-
-
-    open my $fh_sh_2, '>', $seqtk_sh_2 or die;
-    print $fh_sh_2 "date \n";
-    my $command_2 = "$seqtk_exe sample -2 -s100 $fastqc_raw_2 $customer_line | gzip -c > $new_read_2 \n";
-    print $fh_sh_2 $command_2;
-    print $fh_sh_2 "date \n";
-    my $cmd_qsub_sh_2 = "qsub -V -e $sh_log_dir -o $sh_log_dir -S /bin/bash $seqtk_sh_2";
-    system($cmd_qsub_sh_2);
-    print $command_2."\n";
+    my $total_base_gb=changeGbp($total_length)." Gb";
+    my $gc_rate=$total_GC/$total_length*100;$gc_rate=RoundXL($gc_rate,2)."%";
+    my $Nzero_rate=$Nzero_reads/$total_reads*100;$Nzero_rate=RoundXL($Nzero_rate,2)."%";
+    my $N5_rate=$N5_reads/$total_reads*100;$N5_rate=RoundXL($N5_rate,2)."%";
+    my $N_rate=$N_count/$total_length*100;$N_rate=RoundXL($N_rate,2)."%";
+    my $Q30_base=$Q30_base_R1+$Q30_base_R2;
+    my $Q30_rate=$Q30_base/$total_length*100;$Q30_rate=RoundXL($Q30_rate,2)."%";
+    my $Q20_base=$Q20_base_R1+$Q20_base_R2;
+    my $Q20_rate=$Q20_base/$total_length*100;$Q20_rate=RoundXL($Q20_rate,2)."%";
+    print $fh_stat "$tbi_id\t$index\t$total_reads\t$total_length\t$total_base_gb\t$total_GC\t$gc_rate\t$Nzero_reads\t$Nzero_rate\t$N5_reads\t$N5_rate\t$N_count\t$N_rate\t$Q30_base\t$Q30_rate\t$Q20_base\t$Q20_rate\n";
+}
+close ($fh_stat);
+print "End\n"; 
+#############################################################
+#sub
+#############################################################
+sub changeGbp{
+    my $val = shift;
+    $val = $val/1000000000;
+    $val = &RoundXL($val,2);
+    return $val;
 }
 
+sub RoundXL {
+    sprintf("%.$_[1]f", $_[0]);
+}
+
+#sub read_hash {
+#    my $file=shift;
+#    my $hash_ref = shift;
+#    open my $fh, '<:encoding(UTF-8)', $file or die;
+#    while (my $row=<$fh>){
+#        chomp $row;
+#        my $header;
+#        my $value;
+#        my @head_list;
+#        if ($row =~ /^#/) {
+#        next;
+#    }
+#    my $total_reads=`cat 
+#}
+
+sub checkDir{
+    my $dir=shift;
+    if (!-d $dir){
+        die "ERROR! not exist <$dir>\n";
+    }
+}
+
+sub checkFile{
+	my $file = shift;
+	if (!-f $file){
+		die "ERROR ! not found <$file>\n";
+	}
+}
+
+sub read_general_config{
+	my ($file, $hash_ref) = @_;
+	open my $fh, '<:encoding(UTF-8)', $file or die;
+	while (my $row = <$fh>) {
+		chomp $row;
+		if ($row =~ /^#/){ next; } # pass header line
+		if (length($row) == 0){ next; }
+
+		my ($key, $value) = split /\=/, $row;
+		$key = trim($key);
+		$value = trim($value);
+		$hash_ref->{$key} = $value;
+	}
+	close($fh);	
+}
+
+sub trim {
+	my @result = @_;
+
+	foreach (@result) {
+		s/^\s+//;
+		s/\s+$//;
+	}
+
+	return wantarray ? @result : $result[0];
+}
 
 sub make_dir {
     my $dir_name = shift;
     if (!-d $dir_name){
-        system ("mkdir -p $dir_name");
+        my $cmd_mkdir = "mkdir -p $dir_name";
+        system ($cmd_mkdir);
     }
 }
 
-
-sub read_config {
-    my ($file, $hash_ref) = @_;
-    open my $fh, '<:encoding(UTF-8)', $file or die;
-    while (my $row = <$fh>) {
-        chomp $row; 
-        if ($row =~ /^#/) { next; }
-        if ( length $row == 0 ) { next; }
-
-        my ($key, $value) = split /\=/, $row;
-        $key = trim($key);
-        $value = trim($value);
-        $hash_ref->{$key}=$value;
-    }
-    close $fh;
-}
-
-sub trim {
-    my @result = @_;
-    foreach (@result) {
-        s/^\s+//;
-        s/\s+$//;
-    }
-    return wantarray ? @result:$result[0];
-}
-
-
-sub printUsage {
-    print "Usage: perl $0 <in_trim_config> \n";
-    exit;
-}
